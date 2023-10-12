76ffcf7769a34660b5b23911518bcb0699c9f2e4
Alessio <alessionespoli.97@gmail.com>
2023-10-12 10:23:49 +0200

    Added all functions

--- py_utilities/__init__.py
+++ py_utilities/__init__.py
@@ -3,6 +3,8 @@ print(f"Invoking __init__.py for {__name__}")
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy import stats
+from typing import Dict
 
 
 def print_corr_matrix(
@@ -71,3 +73,76 @@ def format_yticks(y, _):
     if y >= 1000:
         return f"{int(y / 1000)}K"
     return str(y)
+
+
+def shapiro_wilk_and_bartlett_tests(
+    df: pd.DataFrame, target_column: str, group_column: str
+) -> Dict[str, Dict[str, float]]:
+    """
+    Perform Shapiro-Wilk test for normality and Bartlett's test for homoscedasticity tests on a DataFrame after removing all nas for selected columns.
+
+    Parameters:
+        df (pd.DataFrame): The input DataFrame.
+        target_column (str): The column containing the target variable.
+        group_column (str): The column containing the group variable.
+
+    Returns:
+        dict: A dictionary containing the results of the Shapiro-Wilk test and Bartlett's test.
+
+    Examples:
+        >>> normality_and_homoscedasticity_tests(df, 'target', 'group')
+    """
+
+    valid_data = df[df[target_column].notna() & df[group_column].notna()]
+
+    # Check for normality using Shapiro-Wilk test for each group
+    shapiro_results = {}
+    groups = valid_data[group_column].unique()
+
+    # Create a single figure with subplots for the Q-Q plots
+    fig, axes = plt.subplots(1, len(groups), figsize=(15, 4))
+
+    for i, group in enumerate(groups):
+        data = valid_data[valid_data[group_column] == group][target_column]
+        shapiro_stat, shapiro_p = stats.shapiro(data)
+        shapiro_results[group] = {
+            "Shapiro Statistic": shapiro_stat,
+            "p-value": shapiro_p,
+        }
+
+        # Create a Q-Q plot in the corresponding subplot
+        stats.probplot(data, plot=axes[i])
+        axes[i].set_title(
+            f"Q-Q Plot for {group} Group in {group_column}\n" f" p-value: {shapiro_p}"
+        )
+        axes[i].set_xlabel("Theoretical Quantiles")
+        axes[i].set_ylabel("Ordered Values")
+
+    # Adjust spacing between subplots
+    plt.tight_layout()
+
+    # Perform Bartlett's test on the filtered data for homoscedasticity
+    bartlett_results = {}
+    group_data = [
+        valid_data[valid_data[group_column] == group][target_column] for group in groups
+    ]
+    bartlett_stat, bartlett_p = stats.bartlett(*group_data)
+    bartlett_results[group_column] = {  # Changed to use group_column as the key
+        "Bartlett Statistic": bartlett_stat,
+        "p-value": bartlett_p,
+    }
+
+    # Print the test results
+    print("Shapiro-Wilk Test Results:")
+    for group, result in shapiro_results.items():
+        print(
+            f"Group: {group}, Shapiro Statistic: {result['Shapiro Statistic']}, p-value: {result['p-value']}"
+        )
+
+    print("\nBartlett Test Results:")
+    for group, result in bartlett_results.items():
+        print(
+            f"Group: {group}, Bartlett Statistic: {result['Bartlett Statistic']}, p-value: {result['p-value']}"
+        )
+
+    return shapiro_results, bartlett_results
